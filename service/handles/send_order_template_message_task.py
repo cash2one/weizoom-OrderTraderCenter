25e52e24b35447d1e6ef14da7a15950056602dff
“chengdegong” <chengdegong@weizoom.com>
2016-12-12 11:46:58 +0800

    cdg

--- service/handles/send_order_template_message_task.py
+++ service/handles/send_order_template_message_task.py
@@ -14,13 +14,22 @@ SELECT * from market_tools_template_message;
 |    7 |          0 | OPENTM207449727-任务完成通知       |            4 | keyword1:task_name,keyword2:prize,keyword3:finish_time                                                   | 2016-01-28 20:53:07 |
 +------+------------+------------------------------------+--------------+----------------------------------------------------------------------------------------------------------+---------------------+
 """
-
+# from django.conf import settings 
+# settings.configure()
 import settings
-from bdem import msgutil
-from business.mall.corporation import Corporation
+from eaglet.core.wxapi import get_weixin_api
 from gaia_conf import TOPIC
 from service.handler_register import register
-from db.mall import models as mall_models
+
+# 消息队列topic
+TOPIC = {
+	'product': 'test-topic',
+	'order': 'test-topic',
+	'delivery_item': 'test-topic',
+	'mall_config': "test-topic",
+	'base_service': 'test-topic',  # 基础的异步化服务，如邮件，模板消息等
+	'template_message': 'test-weixin-topic'  # 微信模板消息
+}
 
 TEMPLATE_DB_TITLE2TMS_NAME = {
 	u"TM00247-购买成功通知": u"购买成功通知",
@@ -31,6 +40,8 @@ TEMPLATE_DB_TITLE2TMS_NAME = {
 	u"TM00853-优惠券过期提醒": u"优惠券过期提醒",
 	u"OPENTM207449727-任务完成通知": u"任务完成通知"
 }
+
+H5_DOMAIN = 'mall.weizoom.com'
 
 
 @register("send_order_template_message_task")
@@ -43,71 +54,133 @@ def process(data, recv_msg=None):
 	type = data['type']
 	if type == 'delivery_item':
 		delivery_item_id = data['delivery_item_id']
+		order_id = delivery_item.origin_order_id
 
-		fill_options = {'with_products': True}
-		delivery_item = corp.delivery_item_repository.get_delivery_item(delivery_item_id, fill_options)
+		# 获取出货单详情
+		data_delivery_item = {
+			'corp_id': corp_id,
+			'id': delivery_item_id
+		}
+		resp_delivery_item = Resource.use('gaia').get({
+			'resource': 'order.delivery_item',
+			'data': data_delivery_item
+		})
+		delivery_item = resp['data']['delivery_item']
 
-		order = corp.order_repository.get_order(delivery_item.origin_order_id, {'with_member': True})
+		# 获取订单详情
+		data_order = {
+			'corp_id': corp_id,
+			'id': order_id
+		}
+		resp_order = Resource.use('gaia').get({
+			'resource': 'mall.template_message',
+			'data': data_order
+		})
+		order = resp['data']['order']
 
+		print '============delivery_item=============',delivery_item
+		print '============order=============',order
 		if to_status == 'shipped':
+			# 获取消息模板
 			send_point = mall_models.PAY_DELIVER_NOTIFY
-			# todo 清理db model
-			template_message_detail = mall_models.MarketToolsTemplateMessageDetail.select().join(
-				mall_models.MarketToolsTemplateMessage).where(
-				mall_models.MarketToolsTemplateMessageDetail.owner == corp_id,
-				mall_models.MarketToolsTemplateMessage.send_point == send_point,
-				mall_models.MarketToolsTemplateMessageDetail.status == 1).first()
+			data_template_message = {
+				'corp_id':corp_id,
+				'send_point':send_point
+			}
+			resp_template_message = Resource.use('gaia').get({
+				'resource': 'mall.template_message',
+				'data': data_template_message
+			})
+			template_message = resp_template_message['data']['template']
+			"""
+			"template": {
+			      "status": 1,
+			      "remark_text": "请您留意最近的快递信息，注意查收。如果有什么问题请第一时间在微信上直接与我们联系，米琦尔感谢您的支持。",
+			      "first_text": "属于您的米琦尔生态富硒大米，已经从岗山硒谷国储级保鲜冷藏仓中取出并且已经发货",
+			      "template_message_id": 2,
+			      "created_at": "2014-12-05 09:49:49",
+			      "id": 32,
+			      "template_id": "toIz4WWt1ek8IY6GFB7UeLHg0uA61c53O0_2yDesi9o",
+			      "owner_id": 102
+		    }
+			"""
 
-			template_message = template_message_detail.template_message
+			name = TEMPLATE_DB_TITLE2TMS_NAME[template_message.get('title','')]
 
-			name = TEMPLATE_DB_TITLE2TMS_NAME[template_message.title]
 
-			url = 'http://%s/mall/order_detail/?woid=%s&order_id=%s' % (
-				settings.H5_DOMAIN, corp, order.bid)
-
-			if template_message_detail:
-				total_sale_price = u'￥%s［实际付款］' % delivery_item.product_statistics_info['total_sale_price']
-				product_names = ','.join(delivery_item.product_statistics_info['product_names'])
-				total_count = delivery_item.product_statistics_info['total_count']
-				if name == u'商品发货通知':
-					items = {
-						'keyword1': delivery_item.express_company_name_text,
-						'keyword2': delivery_item.express_number,
-						'keyword3': product_names,
-						'keyword4': total_count
-
-					}
-				elif name == u'订单标记发货通知':
-					items = {
-						'orderProductPrice': total_sale_price,
-						'orderProductName': product_names,
-						'orderName': order.bid,
-						'orderAddress': delivery_item.ship_address
-
-					}
-				else:
-					items = {}
-
-				data = {
-					'test_env': 'pttest',
-					'user_id': corp.id,
-					'member_id': order.member_info['id'],
-					'name': name,
-					'url': url,
-					'items': items,
-					'first': template_message_detail.first_text,
-					'remark': template_message_detail.remark_text
-
+			template_data = dict()
+			if template_message:
+				member_id = order['member_info']['id']
+				data_member = {
+					'corp_id':corp_id,
+					'member_id':member_id
 				}
-				msgutil.send_message(topic, 'template_msg', data)
+				resp_member = Resource.use('gaia').get({
+					'resource': 'member.member_openid',
+					'data': data_member
+				})
+				openid = resp_member['data']['social_account']['openid']
+				template_data['touser'] = openid
+				template_data['template_id'] = template_message.get('template_id','')
+				template_data['url'] = 'http://%s/mall/order_detail/?woid=%s&order_id=%s' % (settings.H5_DOMAIN, corp_id, order['order_id'])
+				# template_data['url'] = 'http://%s/mall/order_detail/?woid=%s&order_id=%s' % (H5_DOMAIN, corp_id, order['order_id'])
+				template_data['topcolor'] = "#FF0000"
+				detail_data = {}
+				attribute = template_message.get('attribute','')
+				detail_data["first"] = {"value" : template_message.get('first_text',''), "color" : "#000000"}
+				detail_data["remark"] = {"value" : template_message.get('remark_text',''), "color" : "#000000"}
+				order.express_company_name =  u'%s快递' % delivery_item.express_company_name_text
+				if attribute:
+					attribute_data_list = attribute.split(',')
+					for attribute_datas in attribute_data_list:
+						attribute_data = attribute_datas.split(':')
+						key = attribute_data[0].strip()
+						attr = attribute_data[1].strip()
+						if attr == 'final_price' and getattr(order, attr):
+							value = u'￥%s［实际付款］' % getattr(order, attr)
+							detail_data[key] = {"value" : value, "color" : "#173177"}
+						elif hasattr(order, attr):
+							if attr == 'final_price':
+								value = u'￥%s［实际付款］' % getattr(order, attr)
+								detail_data[key] = {"value" : value, "color" : "#173177"}
+							elif attr == 'payment_time':
+								dt = datetime.now()
+								payment_time = dt.strftime('%Y-%m-%d %H:%M:%S')
+								detail_data[key] = {"value" : payment_time, "color" : "#173177"}
+							else:
+								detail_data[key] = {"value" : getattr(order, attr), "color" : "#173177"}
+						else:
+							order_products = order['delivery_item']['products']
+							if 'number' == attr:
+								number = sum([product.count for product in order_products])
+								detail_data[key] = {"value" : number, "color" : "#173177"}
+
+							if 'product_name' == attr:
+								products = self.products
+								product_names =','.join([p.name for p in products])
+								detail_data[key] = {"value" : product_names, "color" : "#173177"}
+				template_data['data'] = detail_data
+
+				# 获取微信用户的access_token
+				data_mpuser_info = {
+					'corp_id':corp_id,
+				}
+				resp_mpuser_info = Resource.use('gaia').get({
+					'resource': 'weixin.mpuser_access_token',
+					'data': data_mpuser_info
+				})
+				mpuser_access_token = resp_mpuser_info['data']['access_token']
+				weixin_api = get_weixin_api(mpuser_access_token)
+    			weixin_api.send_template_message(template_data, True)
+    			print template_data,'======================'
 
 
-# data = {
-#
-# 	'delivery_item_id':'1001435',
-# 	'to_status':'shipped',
-# 	'corp_id':490,
-# 	'type':'delivery_item'
-# }
-# print(2222222222222)
-# process(data,None)
+data = {
+
+	'delivery_item_id':'1001435',
+	'to_status':'shipped',
+	'corp_id':490,
+	'type':'delivery_item'
+}
+print(2222222222222)
+process(data,None)
