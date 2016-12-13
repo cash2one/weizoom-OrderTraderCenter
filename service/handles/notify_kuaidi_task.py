4ea5e1f1daee690582c906af9d1d2e06665cf746
“chengdegong” <chengdegong@weizoom.com>
2016-12-13 16:12:57 +0800

    修改代码

--- service/handles/notify_kuaidi_task.py
+++ service/handles/notify_kuaidi_task.py
@@ -4,22 +4,8 @@
 """
 import time
 
-from eaglet.core.sendmail import sendmail
 from service.handler_register import register
 from eaglet.utils.resource_client import Resource
-
-ORDER_STATUS2NOTIFY_STATUS = {
-	mall_models.ORDER_STATUS_NOT: mall_models.PLACE_ORDER,
-	mall_models.ORDER_STATUS_PAYED_NOT_SHIP: mall_models.PAY_ORDER,
-	mall_models.ORDER_STATUS_PAYED_SHIPED: mall_models.SHIP_ORDER,
-	mall_models.ORDER_STATUS_SUCCESSED: mall_models.SUCCESSED_ORDER,
-	mall_models.ORDER_STATUS_CANCEL: mall_models.CANCEL_ORDER
-}
-
-
-def __send_email(emails, content_described, content):
-	for email in emails:
-		sendmail(email, content_described, content)
 
 
 @register("notify_kuadi_task")
