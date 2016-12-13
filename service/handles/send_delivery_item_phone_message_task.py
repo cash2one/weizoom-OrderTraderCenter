d82655bd5051c26b4cf71a472379938965ac7055
“chengdegong” <chengdegong@weizoom.com>
2016-12-13 15:29:57 +0800

    提交模板消息和短信提醒以及快递订阅的代码

--- service/handles/send_delivery_item_phone_message_task.py
+++ service/handles/send_delivery_item_phone_message_task.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
 """
-处理已支付的订单
+短信发送
 """
-
 from service.handler_register import register
+from eaglet.utils.resource_client import Resource
 
 
 @register("send_delivery_item_phone_message_task")
 def process(data, recv_msg=None):
-	# 暂停
-	pass
-	# corp_id = data['corp_id']
-	# delivery_item_id = data['order_id']
-	#
-	# corp = Corporation(corp_id)
-	#
-	# delivery_item = corp.delivery_item_repository.get_delivery_item(delivery_item_id)
-	#
-	# if delivery_item.has_db_record:
-	#
-	# 	message_content = u"您好，订单号：%s，收货人：%s。已退单，请知晓！【微众传媒】"
-	# 	# 获取手机号
-	#
-	# 	# todo 从出货单读取
-	# 	supplier = corp.supplier_repository.get_supplier(delivery_item.supplier_id)
-	# 	supplier_tel = supplier.supplier_tel
-	#
-	# 	if supplier_tel:
-	# 		send_chargeback_message(supplier_tel, message_content % (delivery_item.bid, delivery_item.ship_name))
+	corp_id = data['corp_id']
+	delivery_item_id = data['delivery_item_id']
+
+	corp = Corporation(corp_id)
+
+	data = {
+		'corp_id': corp_id,
+		'delivery_item_id': delivery_item_id
+	}
+	Resource.use('gaia').put({
+		'resource': 'delivery_item.phone_message',
+		'data': data
+	})
 
