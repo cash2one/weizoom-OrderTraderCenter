8c3f8aea18cb0d1de09e1fb5b6d4c41c3d1b7472
“chengdegong” <chengdegong@weizoom.com>
2016-12-23 10:34:54 +0800

    修改优惠券名称的代码

--- service/handles/order_export_job_task.py
+++ service/handles/order_export_job_task.py
@@ -298,7 +298,12 @@ def handler(data, recv_msg=None):
 						extra_coupon_info = order['extra_coupon_info']
 
 						if extra_coupon_info:
-							coupon_name = extra_coupon_info.get('name', u'无')
+							if extra_coupon_info.get['type'] == "multi_products_coupon":
+								coupon_name = extra_coupon_info['name'] + u'多品券'
+							elif extra_coupon_info.get['type'] == "all_products_coupon":
+								coupon_name = extra_coupon_info['name'] + u'通用券'
+						    else:
+						    	coupon_name = u'无'
 						else:
 							coupon_name = u'无'
 
