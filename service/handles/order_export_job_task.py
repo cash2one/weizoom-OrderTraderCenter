f71f03fa19d6cdb82f27b65a991867d1c501bf2a
朱天琦 <zhutianqi@weizoom.com>
2016-12-23 15:26:38 +0800

    修正导出时的优惠券显示

--- service/handles/order_export_job_task.py
+++ service/handles/order_export_job_task.py
@@ -299,9 +299,9 @@ def handler(data, recv_msg=None):
 
 						if extra_coupon_info:
 							if extra_coupon_info['type'] == "multi_products_coupon":
-								coupon_name = extra_coupon_info['name'] + u'多品券'
+								coupon_name = extra_coupon_info['name'] + u'（多品券）'
 							elif extra_coupon_info['type'] == "all_products_coupon":
-								coupon_name = extra_coupon_info['name'] + u'通用券'
+								coupon_name = extra_coupon_info['name'] + u'（通用券）'
 							else:
 								coupon_name = u'无'
 						else:
