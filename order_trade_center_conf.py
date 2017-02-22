5f0b86d400e9a9670b0dab10ac1255e1194dc5a4
朱天琦 <zhutianqi@weizoom.com>
2017-02-22 14:26:17 +0800

    update

--- order_trade_center_conf.py
+++ order_trade_center_conf.py
@@ -2,10 +2,5 @@
 
 # 消息队列topic
 TOPIC = {
-	'product': 'test-topic',
-	'order': 'test-topic',
-	'delivery_item': 'test-topic',
-	'mall_config': "test-topic",
-	'base_service': 'test-topic',  # 基础的异步化服务，如邮件，模板消息等
-	'template_message': 'test-weixin-topic'  # 微信模板消息
+	'base_service': 'test-topic'  # 基础的异步化服务，如邮件，模板消息等
 }
